d48eea9db8cbd37d39bd09c01ce1403f81fde93a
constantin_k <strongtigerman@gmail.com>
2018-09-17 17:05:46 +0300

    version update 1.3.4.0

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ems-cmd")]
-[assembly: AssemblyCopyright("Copyright © EasyMorph Inc. 2017")]
+[assembly: AssemblyCopyright("Copyright © EasyMorph Inc. 2017-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("6e61eac0-2957-415d-89af-ea675fc244cb")]
 
 
-[assembly: AssemblyVersion("1.3.3.0")]
-[assembly: AssemblyFileVersion("1.3.3.0")]
+[assembly: AssemblyVersion("1.3.4.0")]
+[assembly: AssemblyFileVersion("1.3.4.0")]
