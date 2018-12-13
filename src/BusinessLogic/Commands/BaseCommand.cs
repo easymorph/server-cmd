b83a436102a827e258450de97291f7d7c782aa54
constantin_k <strongtigerman@gmail.com>
2018-12-13 14:04:33 +0200

    1.3.5.1 session opening mechanism  has been changed

--- src/BusinessLogic/Commands/BaseCommand.cs
+++ src/BusinessLogic/Commands/BaseCommand.cs
@@ -39,39 +39,50 @@ namespace MorphCmd.BusinessLogic.Commands
             // call GetSpacesListAsync to retrieve a spaces list and check required space authentication method
             // than open required session
 
-            _output.WriteInfo("Opening session...");
+            _output.WriteInfo("Opening session...");            
 
-            var spacesListResult = await _apiClient.GetSpacesListAsync(_cancellationTokenSource.Token);
-            var desiredSpace = spacesListResult.Items.FirstOrDefault(x => x.SpaceName.Equals(parameters.SpaceName, StringComparison.OrdinalIgnoreCase));
-            if(desiredSpace == null)
+            using (var linkedToken = CancellationTokenSource.CreateLinkedTokenSource(_cancellationTokenSource.Token))
             {
-                throw new Exception($"Server has no space '{parameters.SpaceName}'");
-            }
-            switch (desiredSpace.SpaceAccessRestriction)
-            {
-                case SpaceAccessRestriction.None:
-                    _output.WriteInfo("Method: Anonymous access");
-                    return ApiSession.Anonymous(parameters.SpaceName);                    
-                case SpaceAccessRestriction.BasicPassword when string.IsNullOrWhiteSpace(parameters.Password):
-                    _output.WriteInfo("Method: Password protected space");
-                    throw new Exception($"Space '{parameters.SpaceName}' required password. You should pass '-password' parameter");
-                case SpaceAccessRestriction.BasicPassword:
-                    _output.WriteInfo("Method: Password protected space");
-                    var bpses =  await _apiClient.OpenSessionAsync(parameters.SpaceName, parameters.Password, _cancellationTokenSource.Token);
-                    _output.WriteInfo("Session opened");
-                    return bpses;
-                case SpaceAccessRestriction.WindowsAuthentication:
-                    _output.WriteInfo("Method: Windows authentication");
-                    var wses =await _apiClient.OpenSessionViaWindowsAuthenticationAsync(parameters.SpaceName, _cancellationTokenSource.Token);
-                    _output.WriteInfo("Session opened");
-                    return wses;
-                case SpaceAccessRestriction.NotSupported:
-                default:
-                    throw new Exception("Space access restriction method is not supported by this client.");
-            }
+                linkedToken.CancelAfter(TimeSpan.FromSeconds(20));
+                var token = linkedToken.Token;
 
-            
 
+                var spacesListResult = await _apiClient.GetSpacesListAsync(token);
+                var desiredSpace = spacesListResult.Items.FirstOrDefault(x => x.SpaceName.Equals(parameters.SpaceName, StringComparison.OrdinalIgnoreCase));
+                if (desiredSpace == null)
+                {
+                    throw new Exception($"Server has no space '{parameters.SpaceName}'");
+                }
+                switch (desiredSpace.SpaceAccessRestriction)
+                {
+                    case SpaceAccessRestriction.None:
+                        _output.WriteInfo("Method: Anonymous access");
+                        break;
+                    case SpaceAccessRestriction.BasicPassword when string.IsNullOrWhiteSpace(parameters.Password):
+                        _output.WriteInfo("Method: Password protected space");
+                        throw new Exception($"Space '{parameters.SpaceName}' requires password. You should pass '-password' parameter");
+                    case SpaceAccessRestriction.BasicPassword:
+                        _output.WriteInfo("Method: Password protected space");
+                        break;
+                    case SpaceAccessRestriction.WindowsAuthentication:
+                        _output.WriteInfo("Method: Windows authentication");
+                        break;
+                    case SpaceAccessRestriction.NotSupported when desiredSpace.IsPublic:
+                        _output.WriteInfo("Method: Fall-back anonymous session");
+                        break;
+                    case SpaceAccessRestriction.NotSupported when !desiredSpace.IsPublic & string.IsNullOrWhiteSpace(parameters.Password):
+                        throw new Exception($"Space '{parameters.SpaceName}' requires password. You should pass '-password' parameter");
+                    case SpaceAccessRestriction.NotSupported when !desiredSpace.IsPublic:
+                        _output.WriteInfo("Method: Fall-back password protected space");
+                        break;
+                    default:
+                        throw new Exception("Space access restriction method is not supported by this client.");
+                }
+
+                var session = await _apiClient.OpenSessionAsync(new OpenSessionRequest { SpaceName = parameters.SpaceName, Password = parameters.Password }, token);
+                _output.WriteInfo("Session opened");
+                return session;
+            }
         }
 
     }
