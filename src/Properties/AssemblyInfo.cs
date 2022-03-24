d528e40a3b156edbde80727fc5132b0e5f34e5be
constantin_k <strongtigerman@gmail.com>
2022-03-24 13:25:23 +0200

    version upgrade

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("6e61eac0-2957-415d-89af-ea675fc244cb")]
 
 
-[assembly: AssemblyVersion("5.0.0.1")]
-[assembly: AssemblyFileVersion("5.0.0.1")]
+[assembly: AssemblyVersion("5.2.0.1")]
+[assembly: AssemblyFileVersion("5.2.0.1")]
