b5cd6cef80ec8b780ac7544c55f0e25077e0502d
ynborokh <y.borokh@gmail.com>
2025-10-30 18:06:02 +0200

    Update AssemblyInfo.cs

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("6e61eac0-2957-415d-89af-ea675fc244cb")]
 
 
-[assembly: AssemblyVersion("5.9.1.0")]
-[assembly: AssemblyFileVersion("5.9.1.0")]
+[assembly: AssemblyVersion("5.9.7.0")]
+[assembly: AssemblyFileVersion("5.9.7.0")]
